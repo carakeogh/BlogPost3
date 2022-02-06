ec1362244ecae97476586e7b2405b5e03879f45a
carakeogh <cara.r.keogh@gmail.com>
2022-02-06 01:42:32 -0800

    update

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -12,30 +12,32 @@ class ImdbSpider(scrapy.Spider):
         next_page = response.css("li.ipc-metadata-list__item a").attrib["href"]
 
         if next_page:
-            next_page = response.urljoin(next_page)
+            cast_page = response.urljoin(next_page)
             
-            yield scrapy.Request(next_page, callback = self.parse_full_credits)
+            yield scrapy.Request(cast_page, callback = self.parse_full_credits)
 
     def parse_full_credits(self, response):
         next_credit = [a.attrib["href"] for a in response.css("td.primary_photo a")]
+        #next_credit = response.css("table.cast_list tr")[1:]
 
-        if next_credit:
-            next_credit = response.urljoin(next_credit)
-
-        yield scrapy.Request(next_credit, callback = self.parse_actor_page)
+        for credit in next_credit:
+            if credit:
+                next_link = response.urljoin(credit)
+            
+                yield scrapy.Request(next_link, callback = self.parse_actor_page)
     
     def parse_actor_page(self, response):
-
         name_box = response.css("h1.header")
         actor_name = name_box.css("span.itemprop::text").get()
         
         for film in response.css("div.filmo-category-section b"):
-            movie_or_TV_name = film.css("a::text").get()
+
+            movie_or_TV_name = film.css("::text").get()
 
             if movie_or_TV_name:
                 yield {
                     "actor" : actor_name, 
                     "movie_or_TV_name" : movie_or_TV_name
-                    }
+                }
 
         
